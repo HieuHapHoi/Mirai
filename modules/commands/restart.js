2658c3096b9d51c8f577479572663c0c87683fbb
HieuHapHoi <dgh4151147@gmail.com>
2022-03-04 14:25:06 +0700

    mirai
    mirrai

---
+++ modules/commands/restart.js
@@ -0,0 +1,15 @@
+module.exports.config = {
+	name: "restart",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "manhIT",
+	description: "Khởi động lại Bot",
+	commandCategory: "system",
+	usages: "",
+	cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+	
+	return api.sendMessage(`Đã restart thành công. Vui lòng đợi 1 phút để tiếp tục sử dụng lệnh`, event.threadID, () => process.exit(1));
+}
